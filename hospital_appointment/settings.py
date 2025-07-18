f12932461dc860781e21af50b5147fcbbbd90908
Vinayapriya <vinayapriyakm68@gmail.com>
2025-07-19 00:02:49 +0530

    Added hospital_appointment submodule

--- hospital_appointment/settings.py
+++ hospital_appointment/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'hospital_appointment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'hospital' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,3 +133,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
